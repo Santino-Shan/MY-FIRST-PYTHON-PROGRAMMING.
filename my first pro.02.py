7ba9b25a8f1a1cb62552ff72932593f0954c9742
Santino Shan <shanto.zenity@gmail.com>
2025-11-24 19:22:34 +0600

    Add files via upload

---
+++ my first pro.02.py
@@ -0,0 +1,15 @@
+while True:
+ name = input("What is your Name:")
+ if name == "santino":
+    print("hello,Santino")
+ else:
+    print("Soory,I don't have any information about it")
+    continue
+ password = input("Tell me the password Number:")
+ if password == (1234):
+    print("wellcome,sir")
+ else:
+    print("Incorrect password")
+    continue
+
+
